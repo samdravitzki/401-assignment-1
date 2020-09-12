8522577c51604ce3d27afcdf5a824ab075b47b45
Sam <samdravitzki@gmail.com>
2020-09-12 19:30:47 +1200

    Completed assignment 1

--- cea.py
+++ cea.py
@@ -1,3 +1,5 @@
+# Assignment 1
+
 QMARK = '?'
 NULL = '∅'
 
@@ -24,22 +26,14 @@ def minimal_generalisations(h, x):
             h_new[i] = QMARK if h[i] != NULL else x[i]
     return [tuple(h_new)]
 
-# def minimal_generalisations(h1, h2):
-#     h = h1
-#     for i in range(len(h)):
-#         if not match(h2[i], h1[i]):  # if the elements of h1 and h2 dont match
-#             # if h1[i] == '∅' set h[i] to h2[i], if h1[i] and h2[i] are not equal set h[i] to '?'
-#             h[i] = QMARK if h1[i] != NULL else h2[i]
-#     return h
-
 
 def minimal_specialisation(h, D, x):
     results = []
     for i in range(len(h)):
         if h[i] == QMARK:
-            for feature in D[i]:
-                if x[i] != feature:
-                    h_new = h[:i] + (feature,) + h[i + 1:]
+            for value in D[i]:
+                if x[i] != value:
+                    h_new = h[:i] + (value,) + h[i + 1:]
                     results.append(h_new)
         elif h[i] != NULL:
             h_new = h[:i] + (NULL,) + h[i + 1:]
@@ -67,8 +61,12 @@ def cea_trace(domains, training_examples):
                         # 2) some member of G is more general than h
                         if any([more_general(g, h) for g in G.copy()]):  # fix
                             S.add(h)
+
+                    S.difference_update([s for s in S if
+                                         any([more_general(s, h)
+                                              for h in S if s != h])])
             # remove from S any h that is more general than another hypothesis in S
-            S = {sj for si in S.copy() for sj in S.copy() if more_general(si, sj)}  # fix # only allow hypotheses of equal generality
+            # S = {sj for si in S.copy() for sj in S.copy() if more_general(si, sj)}  # fix # only allow hypotheses of equal generality
         elif not dy:  # if d is negative
             S = {s for s in S.copy() if not match(dx, s)}  # remove from S any hypothesis that match d
             for g in G.copy():  # for each hypothesis g in G that matches d
@@ -77,11 +75,15 @@ def cea_trace(domains, training_examples):
                     for h in minimal_specialisation(g, domains, dx):  # Add to G all minimal specialisations, h, of g such that:
                         # 1) h does not match d
                         # 2) some member of S is more specific than h
-                        a = any([more_general(h, s) for s in S])
-                        if a:
+                        # a = any([more_general(h, s) for s in S])
+                        if any([more_general(h, s) for s in S]):
                             G.add(h)
+
+                    G.difference_update([g for g in G if
+                                         any([more_general(g1, g)
+                                              for g1 in G if g != g1])])
             # remove from G any h that is more specific than another hypothesis in G
-            G = {gj for gi in G.copy() for gj in G.copy() if more_general(gj, gi)}  # only allow hypotheses of equal generality
+            # G = {gj for gi in G.copy() for gj in G.copy() if more_general(gj, gi)}  # only allow hypotheses of equal generality
 
         # Update the traces
         S_trace.append(S.copy())
@@ -97,103 +99,155 @@ def all_agree(S, G, x):
 
 def main():
     # Q1 - CEA Algorithm #
-    # # Example 1
-    # domains = [
-    #     {'red', 'blue'}
-    # ]
-    #
-    # training_examples = [
-    #     (('red',), True)
-    # ]
-    #
-    # S_trace, G_trace = cea_trace(domains, training_examples)
-    # print(len(S_trace), len(G_trace))
-    # print(all(type(x) is set for x in S_trace + G_trace))
-    # S, G = S_trace[-1], G_trace[-1]
-    # print(len(S), len(G))
+    # Example 1
+    domains = [
+        {'red', 'blue'}
+    ]
+
+    training_examples = [
+        (('red',), True)
+    ]
+
+    S_trace, G_trace = cea_trace(domains, training_examples)
+    print(len(S_trace), len(G_trace))
+    print(all(type(x) is set for x in S_trace + G_trace))
+    S, G = S_trace[-1], G_trace[-1]
+    print(len(S), len(G))
 
     # Expect
     # 2 2
     # True
     # 1 1
-
-    # # Example 2
-    # domains = [
-    #     {'T', 'F'}
-    # ]
-    #
-    # training_examples = []  # no training examples
-    #
-    # S_trace, G_trace = cea_trace(domains, training_examples)
-    # print(len(S_trace), len(G_trace))
-    # S, G = S_trace[-1], G_trace[-1]
-    # print(len(S), len(G))
-
-    # Example 3
-    # domains = [
-    #     ('T', 'F'),
-    #     ('T', 'F'),
-    # ]
-    #
-    # training_examples = [
-    #     (('F', 'F'), True),
-    #     (('T', 'T'), False),
-    # ]
-    #
-    # S_trace, G_trace = cea_trace(domains, training_examples)
-    # print(len(S_trace), len(G_trace))
-    # S, G = S_trace[-1], G_trace[-1]
-    # print(len(S), len(G))
-
-    # # Example 4
-    # domains = [
-    #     ('Sunny', 'Cloudy', 'Rainy'),  # Sky
-    #     ('Warm', 'Cold'),  # Temp
-    #     ('Normal', 'High'),  # Humidity
-    #     ('Strong', 'Weak'),  # Wind
-    #     ('Warm', 'Cool'),  # Water
-    #     ('Same', 'Change')  # Forecast
-    # ]
-    #
-    # training_examples = [
-    #     (('Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same'), True),
-    #     (('Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same'), True),
-    #     (('Rainy', 'Cold', 'High', 'Strong', 'Warm', 'Change'), False),
-    #     (('Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change'), True),
-    # ]
-    #
-    # S_trace, G_trace = cea_trace(domains, training_examples)
-
-    # # Example 5
-    # domains = [
-    #     {'red', 'green', 'blue'}
-    # ]
-    #
-    # training_examples = [
-    #     (('red',), True),
-    #     (('green',), True),
-    #     (('blue',), False),
-    # ]
-    #
-    # S_trace, G_trace = cea_trace(domains, training_examples)
-    # S, G = S_trace[-1], G_trace[-1]
-    # print(len(S) == len(G) == 0)
-
-    # Q2 - ALL AGREE #
-
-    # Example 1
+    print()
+    # Example 2
     domains = [
-        {'red', 'blue'},
+        {'T', 'F'}
+    ]
+
+    training_examples = []  # no training examples
+
+    S_trace, G_trace = cea_trace(domains, training_examples)
+    print(len(S_trace), len(G_trace))
+    S, G = S_trace[-1], G_trace[-1]
+    print(len(S), len(G))
+    print()
+    # Example 3
+    domains = [
+        ('T', 'F'),
+        ('T', 'F'),
+    ]
+
+    training_examples = [
+        (('F', 'F'), True),
+        (('T', 'T'), False),
+    ]
+
+    S_trace, G_trace = cea_trace(domains, training_examples)
+    print(len(S_trace), len(G_trace))
+    S, G = S_trace[-1], G_trace[-1]
+    print(len(S), len(G))
+    print()
+    # Example 4
+    domains = [
+        ('Sunny', 'Cloudy', 'Rainy'),  # Sky
+        ('Warm', 'Cold'),  # Temp
+        ('Normal', 'High'),  # Humidity
+        ('Strong', 'Weak'),  # Wind
+        ('Warm', 'Cool'),  # Water
+        ('Same', 'Change')  # Forecast
+    ]
+
+    training_examples = [
+        (('Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same'), True),
+        (('Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same'), True),
+        (('Rainy', 'Cold', 'High', 'Strong', 'Warm', 'Change'), False),
+        (('Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change'), True),
+    ]
+
+    S_trace, G_trace = cea_trace(domains, training_examples)
+    print(S_trace[-1])
+    print(G_trace[-1])
+    print()
+    # # Example 5
+    domains = [
+        {'red', 'green', 'blue'}
     ]
 
     training_examples = [
         (('red',), True),
+        (('green',), True),
+        (('blue',), False),
     ]
 
     S_trace, G_trace = cea_trace(domains, training_examples)
     S, G = S_trace[-1], G_trace[-1]
-    print(all_agree(S, G, ('red',)))
-    print(all_agree(S, G, ('blue',)))
+    print(len(S) == len(G) == 0)
+
+    domains = [{'Y', 'N'} for _ in range(10)]
+
+    def read_training_csv(string):
+        examples = []
+        for line in string.splitlines():
+            *x, label = [value.strip() for value in line.split(',')]
+            y = label == '+'
+            examples.append((x, y))
+        return examples
+
+    training_examples = read_training_csv("""\
+    Y, N, N, N, Y, Y, Y, N, Y, N, -
+    N, N, Y, Y, Y, N, Y, Y, Y, N, -
+    N, N, Y, N, Y, Y, N, Y, Y, Y, -
+    Y, N, Y, N, Y, N, N, N, Y, N, -
+    Y, Y, Y, Y, N, Y, Y, Y, Y, N, -
+    Y, Y, Y, Y, N, N, Y, N, N, N, -
+    Y, N, Y, N, Y, Y, Y, N, Y, N, -
+    Y, N, Y, Y, Y, N, N, Y, N, N, -
+    N, Y, Y, Y, Y, N, N, Y, Y, Y, -
+    Y, Y, Y, N, Y, Y, Y, N, Y, Y, -
+    Y, N, N, N, N, Y, N, Y, N, Y, +
+    Y, N, N, Y, Y, Y, N, N, Y, Y, +
+    N, N, Y, Y, N, N, N, N, Y, N, -
+    Y, N, Y, N, Y, Y, N, N, Y, Y, -""")
+    print(training_examples)
+    S_trace, G_trace = cea_trace(domains, training_examples)
+    print(len(S_trace) == len(G_trace) == 15)
+
+    # Q2 - ALL AGREE #
+
+    # # Example 1
+    # domains = [
+    #     {'red', 'blue'},
+    # ]
+    #
+    # training_examples = [
+    #     (('red',), True),
+    # ]
+    #
+    # S_trace, G_trace = cea_trace(domains, training_examples)
+    # S, G = S_trace[-1], G_trace[-1]
+    # print(all_agree(S, G, ('red',)))
+    # print(all_agree(S, G, ('blue',)))
+    #
+    # domains = [
+    #     {'sunny', 'cloudy', 'rainy'},
+    #     {'warm', 'cold'},
+    #     {'normal', 'high'},
+    #     {'strong', 'weak'},
+    #     {'warm', 'cool'},
+    #     {'same', 'change'},
+    # ]
+    #
+    # training_examples = [
+    #     (('sunny', 'warm', 'normal', 'strong', 'warm', 'same'), True),
+    #     (('sunny', 'warm', 'high', 'strong', 'warm', 'same'), True),
+    #     (('rainy', 'cold', 'high', 'strong', 'warm', 'change'), False),
+    #     (('sunny', 'warm', 'high', 'strong', 'cool', 'change'), True),
+    # ]
+    #
+    # S_trace, G_trace = cea_trace(domains, training_examples)
+    # print(len(S_trace) == len(G_trace) == 5)
+
+
 
     # # Example 2
     # domains = [
--- dt.py
+++ dt.py
@@ -1,3 +1,8 @@
+# Assignment 1
+
+import math
+
+
 class DTNode:
     def __init__(self, decision):
         self.decision = decision
@@ -8,6 +13,12 @@ class DTNode:
             return self.children[self.decision(feature_vector)].predict(feature_vector)
         else:
             return self.decision
+
+    def leaves(self):
+        if self.children is None:
+            return 1
+
+        return sum([child.leaves() for child in self.children])
 
     def is_decision_node(self):
         return True if callable(self.decision) else False
@@ -29,8 +40,6 @@ def partition_by_feature_value(feature_index, dataset):
     return separator, list(p_dataset.values())
 
 
-import math
-
 def find_classes_from_data(data):
     return {c for _, c in data}
 
@@ -47,12 +56,60 @@ def gini(data):
     return sum([proportion_with_k(data, k) * (1 - proportion_with_k(data, k)) for k in find_classes_from_data(data)])
 
 
+# Equal true and false classifications gives an entropy of 1
 def entropy(data):
     return -sum([proportion_with_k(data, k) * math.log(proportion_with_k(data, k)) for k in find_classes_from_data(data)])
 
 
-def train_tree(dataset, misclass):
-    pass
+def impurity_at_node_m(data, feature, criterion):
+    _, feature_partitions = partition_by_feature_value(feature, data)
+    impurity = sum([(len(partition) / len(data)) * criterion(partition) for partition in feature_partitions])
+    return feature, impurity
+
+
+def most_common_label(data):
+    proportion = 0
+    class_label = None
+    for _, c in data:
+        prop_with_c = proportion_with_k(data, c)
+        if prop_with_c > proportion:
+            proportion = prop_with_c
+            class_label = c
+
+    return class_label
+
+
+class DTree:
+    def __init__(self, dataset, criterion):
+        self.dataset = dataset
+        self.criterion = criterion
+        self.features = set(range(len(dataset[0][0])))  # each feature is represented as an index to its data in the dataset
+        self.tree = self.split(self.dataset, self.features)
+
+    def split(self, dataset, features):
+        if proportion_with_k(dataset, dataset[0][1]) == 1:  # if all examples are in one class
+            return DTNode(dataset[0][1])  # return a leaf node with that class label
+        elif len(features) == 0:  # if the set of features is empty
+            # return a leaf node with the most common class label
+            return DTNode(most_common_label(dataset))
+        else:
+            # pick a categorical feature F
+            F, _ = min([impurity_at_node_m(dataset, f, self.criterion) for f in features], key=lambda x: x[1])
+            separator, partitions = partition_by_feature_value(F, dataset)
+            # create new decision node
+            decision_node = DTNode(separator)
+            decision_node.children = [self.split(partition, features - {F}) for partition in partitions]
+            return decision_node
+
+
+# Components of Iterative Dichotomiser 3
+# 1. A way of partitioning the dataset by features;
+# 2. A criterion for impurity of a dataset; and
+# 3. An iterative loop of concurrently splitting the dataset and building the tree.
+def train_tree(dataset, criterion):
+    dtree = DTree(dataset, criterion)
+    return dtree.tree
+
 
 def main():
     # Q3 - DTNode #
@@ -134,17 +191,79 @@ def main():
     # print("{:.4f}".format(entropy(data)))
 
     # Q6 - DTree #
-    dataset = [
-        ((True, True), False),
-        ((True, False), True),
-        ((False, True), True),
-        ((False, False), False)
-    ]
+    # # Example 1
+    # dataset = [
+    #     ((True, True), False),
+    #     ((True, False), True),
+    #     ((False, True), True),
+    #     ((False, False), False)
+    # ]
+    # t = train_tree(dataset, misclassification)
+    # print(t.predict((True, False)))
+    # print(t.predict((False, False)))
+
+    # # Example 2
+    # dataset = [
+    #     (("Sunny", "Hot", "High", "Weak"), False),
+    #     (("Sunny", "Hot", "High", "Strong"), False),
+    #     (("Overcast", "Hot", "High", "Weak"), True),
+    #     (("Rain", "Mild", "High", "Weak"), True),
+    #     (("Rain", "Cool", "Normal", "Weak"), True),
+    #     (("Rain", "Cool", "Normal", "Strong"), False),
+    #     (("Overcast", "Cool", "Normal", "Strong"), True),
+    #     (("Sunny", "Mild", "High", "Weak"), False),
+    #     (("Sunny", "Cool", "Normal", "Weak"), True),
+    #     (("Rain", "Mild", "Normal", "Weak"), True),
+    #     (("Sunny", "Mild", "Normal", "Strong"), True),
+    #     (("Overcast", "Mild", "High", "Strong"), True),
+    #     (("Overcast", "Hot", "Normal", "Weak"), True),
+    #     (("Rain", "Mild", "High", "Strong"), False),
+    # ]
+    # t = train_tree(dataset, misclassification)
+    # print(t.predict(("Overcast", "Cool", "Normal", "Strong")), "Expected: True")
+    # print(t.predict(("Sunny", "Cool", "Normal", "Strong")), "Expected: True")
+    # print()
+
+    # # Example 3
+    from pprint import pprint
+
+    dataset = []
+    with open('car.data', 'r') as f:
+        for line in f.readlines():
+            features = line.strip().split(",")
+            dataset.append((tuple(features[:-1]), features[-1]))
+    pprint(dataset[:5])
+    print(
+        "Exprected: [(('vhigh', 'vhigh', '2', '2', 'small', 'low'), 'unacc'), \n (('vhigh', 'vhigh', '2', '2', 'small', 'med'), 'unacc'), \n (('vhigh', 'vhigh', '2', '2', 'small', 'high'), 'unacc'), \n (('vhigh', 'vhigh', '2', '2', 'med', 'low'), 'unacc'), \n (('vhigh', 'vhigh', '2', '2', 'med', 'med'), 'unacc')]")
     t = train_tree(dataset, misclassification)
-    print(t.predict((True, False)))
-    print(t.predict((False, False)))
+    print(t.predict(("high", "vhigh", "2", "2", "med", "low")), "Expected: unacc")
+    print(t.leaves(), len(dataset), len(dataset) / t.leaves())
 
+    # # Example 4
+    from pprint import pprint
+    dataset = []
+    with open('balance-scale.data', 'r') as f:
+        for line in f.readlines():
+            out, *features = line.strip().split(",")
+            dataset.append((tuple(features), out))
+    pprint(dataset[:5])
+    print(
+        "Expected: [(('1', '1', '1', '1'), 'B'), \n (('1', '1', '1', '2'), 'R'), \n (('1', '1', '1', '3'), 'R'), \n (('1', '1', '1', '4'), 'R'), \n (('1', '1', '1', '5'), 'R')]")
+    t = train_tree(dataset, misclassification)
+    print(t.predict(("1", "4", "3", "2")), "Expected: R")
+    print(t.leaves(), len(dataset), len(dataset) / t.leaves())
 
+    # Q7 - Leaves #
+    # # Example 1
+    # n = DTNode(True)
+    # print(n.leaves())
+    #
+    # # Example 2
+    # t = DTNode(True)
+    # f = DTNode(False)
+    # n = DTNode(lambda v: 0 if not v else 1)
+    # n.children = [t, f]
+    # print(n.leaves())
 
 if __name__ == "__main__":
     main()
