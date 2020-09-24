8376af71a4d030cdf07a642986c941d9c2ada541
Sam <samdravitzki@gmail.com>
2020-09-25 00:44:59 +1200

    Think its working (Logistic Regression)

--- logistic_regression.py
+++ logistic_regression.py
@@ -8,6 +8,8 @@ def sigmoid(x):
 
 def logistic_regression(X, Y, alpha, max_batches):
     # initialise the gradient decent with a vector of zeros called theta
+    intercept = np.ones(X.shape[0])
+    X = np.insert(X, 0, intercept, axis=1)
     xRows, xCols = X.shape
     J = list(range(xCols))
     I = list(range(xRows))
@@ -21,22 +23,22 @@ def logistic_regression(X, Y, alpha, max_batches):
                 yi = Y[i]
                 xij = X[i, j]
 
-                prediction_j = sigmoid(np.matmul(theta.T, xi))
-                theta_j = theta[j] + alpha * (yi - prediction_j) * xij
+                prediction = sigmoid(np.matmul(theta.T, xi))
+                gradient = ((yi - prediction) * xij)
+                theta_j = theta[j] + alpha * gradient
                 theta[j] = theta_j
 
-    return lambda given_input: sigmoid(np.matmul(theta.T, given_input))  # Pretty sure this is correct
+    print("Theta = {}".format(theta))
+    return lambda given_feature: sigmoid(np.matmul(theta.T, np.insert(given_feature, 0, [1])))  # Pretty sure this is correct
 
 
 if __name__ == "__main__":
     # Question 5 #
     xs = np.array([1, 2, 3, 101, 102, 103]).reshape((-1, 1))
     ys = np.array([0, 0, 0, 1, 1, 1])
-    print(xs)
-    print(ys)
     model = logistic_regression(xs, ys, 0.05, 10000)
     test_inputs = np.array([1.5, 4, 10, 20, 30, 40, 50, 60, 70, 80, 90, 101.8, 97]).reshape((-1, 1))
 
     for test_input in test_inputs:
-        val = model(test_input)
-        print("{:.2f}".format(np.array(val).item()))
+        print("{:.2f}".format(np.array(model(test_input)).item()))
+
