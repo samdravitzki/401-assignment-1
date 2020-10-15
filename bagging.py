1a8dfced51f31fb81bc26d0ccecf63d4a1bd1b15
Sam <samdravitzki@gmail.com>
2020-10-15 21:02:00 +1300

    stuff

--- bagging.py
+++ bagging.py
@@ -1,2 +1,30 @@
+import sklearn.datasets
+import sklearn.utils
+import sklearn.tree
+import numpy as np
+from bootstrap import bootstrap
+from ensemble import voting_ensemble
 
 
+def bagging_model(learner, dataset, n_models, sample_size):
+    bootstrap_data = bootstrap(dataset, sample_size)
+    models = [learner(next(bootstrap_data)) for _ in range(n_models)]
+    return voting_ensemble(models)
+
+
+if "__main__" == __name__:
+    iris = sklearn.datasets.load_iris()
+    data, target = sklearn.utils.shuffle(iris.data, iris.target, random_state=1)
+    train_data, train_target = data[:-5, :], target[:-5]
+    test_data, test_target = data[-5:, :], target[-5:]
+    dataset = np.hstack((train_data, train_target.reshape((-1, 1))))
+
+    def tree_learner(dataset):
+        features, target = dataset[:, :-1], dataset[:, -1]
+        model = sklearn.tree.DecisionTreeClassifier(random_state=1).fit(features, target)
+        return lambda v: model.predict(np.array([v]))[0]
+
+    bagged = bagging_model(tree_learner, dataset, 50, len(dataset)//2)
+    # Note that we get the first one wrong!
+    for (v, c) in zip(test_data, test_target):
+        print(int(bagged(v)), c)
--- bootstrap.py
+++ bootstrap.py
@@ -27,6 +27,7 @@ def random_feature(D, random_iterator):
     return D[int(num_features * random)]
 
 
+
 def bootstrap(D, sample_size):
     random = pseudo_random()
 
@@ -38,12 +39,50 @@ def bootstrap(D, sample_size):
         yield np.array(new_dataset)
 
 
-if "__main__" == __name__:
-    dataset = np.array([[1, 0, 2, 3],
-                        [2, 3, 0, 0],
-                        [4, 1, 2, 0],
-                        [3, 2, 1, 0]])
+class weighted_bootstrap:
+    def __init__(self, dataset, weights, sample_size):
+        self.dataset = dataset
+        self.weights = weights
+        self.sample_size = sample_size
+        self.random = pseudo_random()
 
-    ds_gen = bootstrap(dataset, 3)
-    print(next(ds_gen))
-    print(next(ds_gen))
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        new_dataset = []
+        total_weights = sum(self.weights)
+
+        for _ in range(self.sample_size):
+            pick_chance = next(take(1, self.random)) * total_weights
+            running_total = 0
+            # Choose random number
+            for i in range(len(self.weights)):
+                running_total += self.weights[i]
+
+                if running_total > pick_chance:
+                    new_dataset.append(self.dataset[i])
+                    break
+
+        return np.array(new_dataset)
+
+
+if "__main__" == __name__:
+    # Question 2
+    # dataset = np.array([[1, 0, 2, 3],
+    #                     [2, 3, 0, 0],
+    #                     [4, 1, 2, 0],
+    #                     [3, 2, 1, 0]])
+    #
+    # ds_gen = bootstrap(dataset, 3)
+    # print(next(ds_gen))
+    # print(next(ds_gen))
+
+    # Question 4
+    wbs = weighted_bootstrap([1, 2, 3, 4, 5], [1, 1, 1, 1, 1], 5)
+    print(next(wbs))
+    print(next(wbs))
+    print()
+    wbs.weights = [1, 1, 1000, 1, 1]
+    print(next(wbs))
+    print(next(wbs))
